15a691e57d6d86ecb7c2731f30461f63b98d4581
suozhang333 <2396266119@qq.com>
2024-07-05 16:51:49 +0800

    Works to be continued

--- character_body_2d_starter/basic_movement.gd
+++ character_body_2d_starter/basic_movement.gd
@@ -1,12 +1,11 @@
 extends CharacterBody2D
-@export var speed = 400
+
+var speed = 300
 
 func get_input():
-	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_dir * speed
 
 func _physics_process(delta):
 	get_input()
 	move_and_collide(velocity * delta)
-
-
