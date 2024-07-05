d93d931f6b334846e9e64c9171eeb7afb637e307
suozhang333 <2396266119@qq.com>
2024-07-05 09:10:35 +0800

    Demo with player and simple kitchen layout

--- Xgame_Maoge_shishi/player.gd
+++ Xgame_Maoge_shishi/player.gd
@@ -1,28 +1,11 @@
-extends Area2D
+extends CharacterBody2D
+@export var speed = 400
 
-@export var speed = 400 # How fast the player will move (pixels/sec).
-var screen_size # Size of the game window.
+func get_input():
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_dir * speed
 
-func _ready():
-	screen_size = get_viewport_rect().size
-
-func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop()
-	
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+func _physics_process(delta):
+	get_input()
+	move_and_collide(velocity * delta)
 
