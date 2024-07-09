ce69624a78475747b79718fc88a4cef166cce94e
suozhang333 <2396266119@qq.com>
2024-07-09 17:04:25 +0800

    picking up chicken leg

--- Xgame_Maoge_shishi/player.gd
+++ Xgame_Maoge_shishi/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
+
 @export var speed = 400
+@onready var player=$Player
 
 func get_input():
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -9,4 +11,7 @@ func _physics_process(delta):
 	get_input()
 	move_and_collide(velocity * delta)
 
-
+func _on_body_entered(body):
+	if body.is_in_group("pickable"):
+		body.queue_free()
+		print("item picked up!")
