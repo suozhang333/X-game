670313d162479ce296b517eb4fb798e6a2435c4d
unknown <2396266119@qq.com>
2024-07-09 23:43:17 +0800

    sorting out documents and feature carry_the_chicken_leg achieved

---
+++ Maoge_Demo_Projects/Xgame_Maoge_shishi/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+@export var speed = 400
+# 实例变量  
+var item: Node = null  
+
+func get_input():
+	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = input_dir * speed
+
+func _physics_process(_delta):
+	get_input()
+	move_and_collide(velocity * _delta)
+	# 如果物品存在，则更新其位置以跟随人物
+	#if item and item.is_inside_tree():
+		## 更新位置
+		#item.position = self.position
+
+func _on_body_entered(body):
+	if body.name == "Item" and not item:
+		item = body
+		item.get_parent().remove_child(item) # 从原父节点移除
+		add_child(item) # 添加到人物节点下
+		# 设置物品的位置跟随在人物左上方
+		var offset = Vector2(-250, -210) # 20 向右, -50 向上（因为 Y 轴是向下的）  
+		item.position = self.position + offset
+
+	
+	
