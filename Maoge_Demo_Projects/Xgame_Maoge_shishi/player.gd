edefa5312a5d7616428943ec6e160b714fcfeb86
suozhang333 <2396266119@qq.com>
2024-07-11 16:07:14 +0800

    player.gd modified

--- Maoge_Demo_Projects/Xgame_Maoge_shishi/player.gd
+++ Maoge_Demo_Projects/Xgame_Maoge_shishi/player.gd
@@ -11,19 +11,30 @@ func get_input():
 func _physics_process(_delta):
 	get_input()
 	move_and_collide(velocity * _delta)
-	# 如果物品存在，则更新其位置以跟随人物
-	#if item and item.is_inside_tree():
-		## 更新位置
-		#item.position = self.position
 
 func _on_body_entered(body):
-	if body.name == "Item" and not item:
-		item = body
-		item.get_parent().remove_child(item) # 从原父节点移除
-		add_child(item) # 添加到人物节点下
-		# 设置物品的位置跟随在人物左上方
-		var offset = Vector2(-250, -210) # 20 向右, -50 向上（因为 Y 轴是向下的）  
-		item.position = self.position + offset
+	#if body.name == "Item" and not item:
+		#item = body
+		#item.get_parent().remove_child(item) # 从原父节点移除
+		#add_child(item) # 添加到人物节点下
+		#var collision_shape = body.get_node("CollisionShape2D")
+		#if collision_shape:
+			#collision_shape.set_deferred("disabled",true)
+		#$CollisionShape2D.set_deferred("disabled",true)
+		#$Area2DPlayer/CollisionShape2D.set_deferred("disabled",true)
 
-	
+		
+		# 设置物品的位置跟随在人物左上方
+		#var offset = Vector2(-250, -210) # 20 向右, -50 向上（因为 Y 轴是向下的）  
+		#item.position = self.position + offset
+
+		if body is Node:
+			call_deferred("handle_body_entered",body)
+
+func handle_body_entered(body):
+	var parent = get_parent()
+	if parent:
+		parent.remove_child(body)
+		add_child(body)
+
 	
