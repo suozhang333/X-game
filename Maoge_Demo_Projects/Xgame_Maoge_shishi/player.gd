6a45c805f50b64429fd5b18831684d851a95b81e
suozhang333 <2396266119@qq.com>
2024-07-11 16:25:44 +0800

    player.gd ready to use

--- Maoge_Demo_Projects/Xgame_Maoge_shishi/player.gd
+++ Maoge_Demo_Projects/Xgame_Maoge_shishi/player.gd
@@ -13,28 +13,12 @@ func _physics_process(_delta):
 	move_and_collide(velocity * _delta)
 
 func _on_body_entered(body):
-	#if body.name == "Item" and not item:
-		#item = body
-		#item.get_parent().remove_child(item) # 从原父节点移除
-		#add_child(item) # 添加到人物节点下
-		#var collision_shape = body.get_node("CollisionShape2D")
-		#if collision_shape:
-			#collision_shape.set_deferred("disabled",true)
-		#$CollisionShape2D.set_deferred("disabled",true)
-		#$Area2DPlayer/CollisionShape2D.set_deferred("disabled",true)
-
-		
+	if body.name == "Item" and not item:
+		item = body
+		item.get_parent().remove_child(item) # 从原父节点移除
+		add_child(item) # 添加到人物节点下
 		# 设置物品的位置跟随在人物左上方
-		#var offset = Vector2(-250, -210) # 20 向右, -50 向上（因为 Y 轴是向下的）  
-		#item.position = self.position + offset
-
-		if body is Node:
-			call_deferred("handle_body_entered",body)
-
-func handle_body_entered(body):
-	var parent = get_parent()
-	if parent:
-		parent.remove_child(body)
-		add_child(body)
+		var offset = Vector2(-250, -210) # 20 向右, -50 向上（因为 Y 轴是向下的）  
+		item.position = self.position + offset
 
 	
